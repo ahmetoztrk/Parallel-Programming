44b08a04a27d0b47c9bb1a8fb2aab055afc8f85b
ahmetoztrk <ahmetoztrk1709@gmail.com>
2024-02-03 23:34:28 +0300

    added mpiSend

---
+++ mpi/mpiSend.c
@@ -0,0 +1,41 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int rank, size;
+    int tag, destination, count;
+    int buffer; 
+
+    tag = 1234;
+    destination = 1; 
+    count = 1; 
+    MPI_Status status;
+    MPI_Request request = MPI_REQUEST_NULL;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+
+    if (rank == 0) {
+        buffer = 9999;
+        MPI_Isend(&buffer, count, MPI_INT, destination, tag, MPI_COMM_WORLD, &request); 
+    }
+
+    if (rank == destination) {
+        MPI_Irecv(&buffer, count, MPI_INT, 0, tag, MPI_COMM_WORLD, &request); 
+    }
+    MPI_Wait(&request, &status); 
+
+    if (rank == 0) {
+        printf("processor %d sent %d\n", rank, buffer);
+    }
+    if (rank == destination) {
+        printf("processor %d rcv %d\n", rank, buffer);
+    }
+
+    MPI_Finalize();
+
+	return 0;
+}
